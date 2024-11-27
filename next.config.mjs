83f2151276f38037467d3fca1d3902b4b5567663
Ivan <ivan.filmart@gmail.com>
2024-11-27 20:10:16 +0300

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,30 @@ const nextConfig = {
   reactStrictMode: true,
   publicRuntimeConfig: {
     API_URL: process.env.API_URL,
-    TELEGRAM_BOT_TOKEN : process.env.TELEGRAM_BOT_TOKEN, 
-    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID
-  }
+    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
