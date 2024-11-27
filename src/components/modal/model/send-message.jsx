eb1c9a4b9cd7c915918cb3583716e2b5eff3ae3f
Ivan <ivan.filmart@gmail.com>
2024-11-27 19:51:37 +0300

    out

--- src/components/modal/model/send-message.jsx
+++ src/components/modal/model/send-message.jsx
@@ -1,27 +1,58 @@
 import { ConfigServices } from "@/services/config";
-//import TelegramBot from 'node-telegram-bot-api';
 
-export default async function SenderTelegram( req ){
-    const data =  req.body;     
-    const name = req.get("name");
-    const phone = req.get("tel");
-    const comments = req.get("comments");
-    const [TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID] = ConfigServices.getENV();
-    const text = `
+export default async function SenderTelegram(req) {
+  const data = req.body;
+  const name = req.get("name");
+  const phone = req.get("tel");
+  const comments = req.get("comments");
+  const [TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID] = ConfigServices.getENV();
+  const text = `
       Новая заявка с сайта:\n 
       Имя: ${name} \n
       Телефон: ${phone} \n
       Комментарий: ${comments || "без комментария"}
     `;
 
-    alert(text);
+  await fetch(
+    "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendMessage",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chat_id: TELEGRAM_CHAT_ID,
+        text,
+      }),
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Message sent successfully:", data);
+    })
+    .catch((error) => {
+      console.error("Error sending message:", error);
+    });
 
-    // const response = await fetch('', {
-    //     method: "post",
-    //     body: text,
-    // });
+  // fetch('https://api.telegram.org/bot' + TELEGRAM_BOT_TOKEN + '/sendMessage', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //       chat_id: TELEGRAM_CHAT_ID,
+  //       text
+  //     })
+  //   })
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log('Message sent successfully:', data);
+  //   })
+  //   .catch(error => {
+  //     console.error('Error sending message:', error);
+  //   });
 
-    // const Bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
-    // Bot.sendMessage(TELEGRAM_CHAT_ID, text);
-    // return res.status(200).json({ok: true, message: "Заявка успешно отправлена"});
+  // const Bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
+  // Bot.sendMessage(TELEGRAM_CHAT_ID, text);
+  // return res.status(200).json({ok: true, message: "Заявка успешно отправлена"});
 }
