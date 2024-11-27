d2cfb6f97cacb3923a4af815d670c6a8aaa26a75
Ivan <ivan.filmart@gmail.com>
2024-11-27 20:22:40 +0300

    cors

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,9 @@
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
-const { API_URL, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID } = publicRuntimeConfig;
+const { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID } = publicRuntimeConfig;
 
 export const ConfigServices = {
   getENV() {
-    return [ API_URL, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID];
+    return [ TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID];
   },
 };
