f9fc521f817bdc1d12b4476fc7589cdb239824a6
Ivan <ivan.filmart@gmail.com>
2024-11-27 19:25:39 +0300

    Работа с API

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     API_URL: process.env.API_URL,
     TELEGRAM_BOT_TOKEN : process.env.TELEGRAM_BOT_TOKEN, 
     TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID
-  },
+  }
 };
 
 export default nextConfig;
