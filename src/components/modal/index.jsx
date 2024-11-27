852600c311442dc5c97ca4e902ff51026066c94d
Ivan <ivan.filmart@gmail.com>
2024-11-27 22:19:10 +0300

    Добавлены уведомления к форме

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -17,10 +17,36 @@ export default function Modal({
     setIsLoading(true);
     event.preventDefault();
     const formData = new FormData(event.target);
-
-      const result = await SenderTelegram(formData);
-      const status = await result.ok;
-      if (status) {
+    const name = formData.get("name");
+    const phone = formData.get("tel");
+    const comments = formData.get("comments");
+    const [TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID] = ConfigServices.getENV();
+    const text = `
+        Новая заявка с сайта:\n 
+        Имя: ${name} \n
+        Телефон: ${phone} \n
+        Комментарий: ${comments || "без комментария"}
+      `;
+  
+    await fetch(
+      "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendMessage",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST",
+          "Access-Control-Allow-Headers":
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        },
+        body: JSON.stringify({
+          chat_id: TELEGRAM_CHAT_ID,
+          text,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
         setIsLoading(false);
         setResultState((result) => {
           return {
@@ -33,7 +59,8 @@ export default function Modal({
         });
         changeModal(false);
         setStateModalNotification(true);
-      } else {
+      })
+      .catch((error) => {
         setIsLoading(false);
         setResultState((result) => {
           return {
@@ -44,8 +71,7 @@ export default function Modal({
           };
         });
         setStateModalNotification(true);
-      }
-
+      });
   }
 
   return (
