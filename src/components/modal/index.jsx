82b9fd3d31927017a3dae8483540aacc9d5fb640
Ivan <ivan.filmart@gmail.com>
2024-11-29 13:18:13 +0300

    Исправление в модальном окне с формой

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -5,6 +5,7 @@ import FullModal from "../ui/modal-full";
 import Loader from "./ui/loader";
 import { ConfigServices } from "@/services/config";
 import clsx from 'clsx';
+import axios from 'axios';
 // todo: Рефакторинг
 
 export default function Modal({
@@ -13,14 +14,14 @@ export default function Modal({
   setResultState,
   setStateModalNotification,
 }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fileState, setFileState] = useState({
+  const [isLoadingModal, setIsLoadingModal] = useState(false);
+  const [fileStateModal, setFileStateModal] = useState({
     ok: false,
     name: ".jpg/png, .pdf, .docx, .xlsx",
   });
 
-  async function action(event) {
-    setIsLoading(true);
+  async function actionModal(event) {
+    setIsLoadingModal(true);
     event.preventDefault();
     const formData = new FormData(event.target);
     const name = formData.get("name");
@@ -28,21 +29,21 @@ export default function Modal({
     const comments = formData.get("comments");
     const files = formData.get("file");
 
-// Локальная проверка формы на размер файла
-if (files.size > 52000000) {
-  setIsLoading(false);
-  setResultState((result) => {
-    return {
-      ...result,
-      ok: "false",
-      title: "Ошибка в файле",
-      description:
-        "Размер файла превысил допустимые 50мб. Попробуйте отправить его нам в чат Telegram",
-    };
-  });
-  setStateModalNotification(true);
-  return false;
-}
+    // Локальная проверка формы на размер файла
+    if (files.size > 52000000) {
+      setIsLoadingModal(false);
+      setResultState((result) => {
+        return {
+          ...result,
+          ok: "false",
+          title: "Ошибка в файле",
+          description:
+            "Размер файла превысил допустимые 50мб. Попробуйте отправить его нам в чат Telegram",
+        };
+      });
+      setStateModalNotification(true);
+      return false;
+    }
 
     const [TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID] = ConfigServices.getENV();
     const text = `
@@ -73,16 +74,14 @@ if (files.size > 52000000) {
       .then((data) => {
         // Если отправилось, отправляем следом файл
         if (files.name) {
-          const formDto = new FormData();
-          formDto.append("chat_id", TELEGRAM_CHAT_ID);
-          formDto.append("document", files);
-          console.log(formDto);
-          axios
-            .post(
+          const formDtoModal = new FormData();
+          formDtoModal.append("chat_id", TELEGRAM_CHAT_ID);
+          formDtoModal.append("document", files);
+          axios.post(
               "https://api.telegram.org/bot" +
                 TELEGRAM_BOT_TOKEN +
                 "/sendDocument",
-              formDto,
+              formDtoModal,
               {
                 headers: {
                   "Content-Type": "multipart/form-data",
@@ -90,7 +89,7 @@ if (files.size > 52000000) {
               }
             )
             .then((el) => {
-              setIsLoading(false);
+              setIsLoadingModal(false);
               setResultState((result) => {
                 return {
                   ...result,
@@ -103,7 +102,7 @@ if (files.size > 52000000) {
               setStateModalNotification(true);
             });
         } else {
-          setIsLoading(false);
+          setIsLoadingModal(false);
           setResultState((result) => {
             return {
               ...result,
@@ -117,7 +116,7 @@ if (files.size > 52000000) {
         }
       })
       .catch((error) => {
-        setIsLoading(false);
+        setIsLoadingModal(false);
         setResultState((result) => {
           return {
             ...result,
@@ -130,8 +129,8 @@ if (files.size > 52000000) {
       });
   }
 
-  function handleFile(event) {
-    setFileState((oldState) => {
+  function handleFileModal(event) {
+    setFileStateModal((oldState) => {
       return {
         ...oldState,
         ok: true,
@@ -144,71 +143,71 @@ if (files.size > 52000000) {
     <FullModal
       title="Получить КП"
       body={
-        <form onSubmit={action}>
+        <form onSubmit={actionModal}>
           <div className="px-6 pb-5">
             <label
-              htmlFor="formName"
+              htmlFor="formNameModal"
               className="text-slate-50 text-lg font-normal"
             >
               ваше имя
             </label>
             <input
-              id="formName"
+              id="formNameModal"
               type="text"
-              name="name"
+              name="nameModal"
               className="bg-[#D9D9D9] rounded-lg border-0 w-full mt-2 mb-5 p-5"
               required
             />
             <label
-              htmlFor="formPhone"
+              htmlFor="formPhoneModal"
               className="text-slate-50 text-lg font-normal"
             >
               номер телефона
             </label>
             <input
-              id="formPhone"
+              id="formPhoneModal"
               type="tel"
-              name="tel"
+              name="telModal"
               className="bg-[#D9D9D9] rounded-lg border-0 w-full mt-2 mb-5 p-5"
               required
             />
             <label
-              htmlFor="formComment"
+              htmlFor="formCommentModal"
               className="text-slate-50 text-lg font-normal"
             >
               дополнительный комментарий
             </label>
             <input
-              id="formComment"
+              id="formCommentModal"
               type="text"
-              name="comment"
+              name="commentModal"
               className="bg-[#D9D9D9] rounded-lg border-0 w-full mt-2 mb-5 p-5"
             />
             <p className="text-slate-50 text-lg font-normal">
               прикрепите файл (не более 50 мб.)
               <br />
-              {fileState.name}
+              {fileStateModal.name}
             </p>
             <label
-              htmlFor="formFile"
+              htmlFor="formFileModal"
               className={clsx(
                 "rounded-lg border flex items-center justify-center w-full mt-2 p-5 border-dashed border-[#585858] cursor-pointer hover:bg-[#525252] transition-colors",
-                fileState.ok ? "bg-green-800" : "bg-[#444444]"
+                fileStateModal.ok ? "bg-green-800" : "bg-[#444444]"
               )}
             >
-              {fileState.ok ? (
+              {fileStateModal.ok ? (
                 <UIicons.ok className="w-7 h-7 text-slate-50" />
               ) : (
                 <UIicons.fileDownloading className="w-7 h-7 text-[#D9D9D9]" />
               )}
             </label>
 
-            <input id="formFile" type="file" name="files" className="hidden" onChange={handleFile}/>
+            <input id="formFileModal" type="file" name="filesModal" className="hidden" onChange={handleFileModal}/>
           </div>
 
           <div className="border-t border-dashed border-[#6d6d6d] box-border mx-auto p-6">
             <UIbuttonSender
-              title={isLoading ? <Loader /> : "ОТПРАВИТЬ"}
+              title={isLoadingModal ? <Loader /> : "ОТПРАВИТЬ"}
               type="submit"
               handleClick={() => {}}
               className="w-full"
